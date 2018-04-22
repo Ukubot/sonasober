88ba26c04d4db1d9ce698f6e8c2870056aaf697a
joonsuu <36069242+joonsuu@users.noreply.github.com>
2018-04-22 14:33:53 +0300

    Update scripts.js
    Scroll animations to up-arrow and input area

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 
+
 //Feature to click elements only by hovering at them / looking at them
 function visiontracker(){
   var delay=3000, setTimeoutConst;
@@ -12,21 +13,58 @@ function visiontracker(){
 })
 };
 
-
-
 //A element on the site that plays the sounds we feed it
-var playSound = function(soundFile) {
+var playSound = function(soundFile,soundFile2) {
   $("#sound").html("<embed src=\"psv/" + soundFile + ".wav" + "\" hidden=\"true\" autostart=\"true\" />");
+    
 };
+
+
+function playAudio(soundFile, soundFile2){
+    
+    var audio =  soundFile;
+    var audio2 = soundFile2;
+    
+    if(audio2 !== undefined){
+        
+        
+        $.playSound(audio);
+        
+        $("#sound").addEventListener('ended', function() {
+            $.playSound(audio2);
+        });
+    }
+    
+    
+    //audio1.onended();
+    //var audio2 = $("#sound2").html("<embed src=\"psv/" + soundFile2 + ".wav" + "\" hidden=\"true\" />");  
+};
+
+//audio2.pause();
+    
+
+
+
+
+
+
+//(function(callback){ console.log(callback.toString()); })(playTwoSounds);
 
 //Getting data from the JSON file and making word-element components with the information
 $(function() {
   $.getJSON("mydata.json", function(data) {
     var html = '';
+    
     $.each(data, function(key, value){
-      var sound = '\''+value.sound+ '\'';
+    
+            var sound = '\'' +value.sound+ '\'';
+            console.log(sound);
+            var sound2 = '\'' +value.sound2+ '\'';
+            console.log(sound2);
+        
       html += '<div id="'+value.category+'" class="category-filter">';
-      html += '<div id="'+value.id+'" class="word-element img-button" value="'+value.name+'" onclick="playSound('+sound+')">';
+      
+      html += '<div id="'+value.id+'" class="word-element img-button" value="'+value.name+'" onclick="playSound('+sound+','+sound2+')">';
       html += '<div class="word-element-inner">';
       html += '<img src="images/'+value.image+'.png"/>';
       html += '<div class="image-title">'+value.name+'</div>';
@@ -66,7 +104,35 @@ $(function(){
   });
 });
 
+//HIDE TOP ARROW WHEN SCROLLED TO TOP
+$(function(){
+$('div#topic-menu').scroll(function() {    
+    var scroll = $('div#topic-menu').scrollTop();
+    if (scroll == 0) {
+        $(".arrow__arrow-up").hide();
+    }
+    
+    else {
+        $(".arrow__arrow-up").show();
+    }
+});
 
+});
+
+// STICKY MENU WHEN SROLLED
+$(function(){
+$('div#word-cards').scroll(function() {    
+    var scroll = $('div#word-cards').scrollTop();
+    if (scroll >= 50) {
+        $(".topmenu-container").addClass("topmenu-container-fixed");
+    }
+    
+    else {
+        $(".topmenu-container").removeClass("topmenu-container-fixed");
+    }
+});
+
+});
 
 
 // Arrows for scrolling topic elements up and down
