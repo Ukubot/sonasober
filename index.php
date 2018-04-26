126ea5a78039a851742c829f4590866130791ee5
prliisa <l.prits@gmail.com>
2018-04-26 16:11:47 +0300

    Update index.php
    Add mobile CSS file

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="styles.css">
     <link href="/fonts/OpenDyslexic-Regular.otf">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="mobile.css">
 
     <style type="text/css">
 @font-face {
